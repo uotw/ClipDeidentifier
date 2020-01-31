56eedba210fcf8386704772b3cb2d79312428459
Ben <bensmith.md@gmail.com>
2020-01-31 17:05:29 -0500

    fixed icon building code

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 sudo rm -rf Clip\ Deidentifier-darwin-x64
-electron-packager . "Clip Deidentifier" --platform=darwin --arch=x64 --overwrite --ignore=bin/win
-#cp icon.icns Clip\ Deidentifier-darwin-x64/Clip\ Deidentifier.app/Contents/Resources/electron.icns
+electron-packager . "Clip Deidentifier" --platform=darwin --arch=x64 --overwrite --ignore=bin/win 
+cp icon.icns Clip\ Deidentifier-darwin-x64/Clip\ Deidentifier.app/Contents/Resources/electron.icns
 #touch Clip\ Deidentifier-darwin-x64/Clip\ Deidentifier.app/
 open  Clip\ Deidentifier-darwin-x64
