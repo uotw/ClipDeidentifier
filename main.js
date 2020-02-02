481dc543052a1ebc57787613e7106c8f117560a6
Ben <bensmith.md@gmail.com>
2020-02-01 19:29:56 -0500

    fixing windows code

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const electron = require('electron')
 // Module to control application life.
 const app = electron.app
+const fs = require('fs')
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 const windowStateKeeper = require('electron-window-state');
