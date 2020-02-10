cd0e30bfc0560b810c8e37cf8309233c408a590a
Ben <bensmith.md@gmail.com>
2020-02-10 08:56:09 -0500

    added error control to FF downloads

--- js/renderer.js
+++ js/renderer.js
@@ -44,87 +44,116 @@ function focusThisApp() {
 }
 
 function downloadFFmpeg(callback) {
-  function tickerFn(data) {
-    //console.log('\x1b[2m' + data.filename + ': Downloading ' + (data.progress * 100).toFixed(1) + '%\x1b[0m');
-    var elem = document.getElementById("myBar");
-    var percnum = (data.progress * 100/2).toFixed(1);
-    var perc = percnum + '%';
-    elem.style.width = perc;
-    $('#label').html(Math.round(percnum)+'%');
-  }
-  ffbinaries.clearCache();
+    $("#progressmsg").html('getting started: downloading FFmpeg binaries now...');
+    function tickerFn(data) {
+        //console.log('\x1b[2m' + data.filename + ': Downloading ' + (data.progress * 100).toFixed(1) + '%\x1b[0m');
+        var elem = document.getElementById("myBar");
+        var percnum = (data.progress * 100 / 2).toFixed(1);
+        var perc = percnum + '%';
+        elem.style.width = perc;
+        $('#label').html(Math.round(percnum) + '%');
+    }
+    //ffbinaries.clearCache();
 
-  var plat = ffbinaries.detectPlatform();
+    var plat = ffbinaries.detectPlatform();
 
-  var options = {
-    platform: plat,
-    quiet: false,
-    destination: ffpath,
-    tickerFn: tickerFn,
-    tickerInterval: 100
-  };
+    var options = {
+        platform: plat,
+        quiet: false,
+        destination: ffpath,
+        tickerFn: tickerFn,
+        tickerInterval: 100
+    };
 
-  ffbinaries.downloadFiles(['ffmpeg'], options, function (err, data) {
-    console.log('Downloading ffmpeg binary to ' + ffpath + '.');
-    console.log('err', err);
-    console.log('data', data);
-
-    callback(err, data);
-  });
+    ffbinaries.downloadFiles(['ffmpeg'], options, function(err, data) {
+        console.log('Downloading ffmpeg binary to ' + ffpath + '.');
+        console.log('err', err);
+        console.log('data', data);
+        if (err != null) {
+            console.log('err', err);
+        }
+        callback(err, data);
+    });
 }
 
 function downloadFFprobe(callback) {
-  function tickerFn(data) {
-    //console.log('\x1b[2m' + data.filename + ': Downloading ' + (data.progress * 100).toFixed(1) + '%\x1b[0m');
-    var elem = document.getElementById("myBar");
-    var percnum = (data.progress * 100/2 + 50).toFixed(1);
-    var perc = percnum + '%';
-    elem.style.width = perc;
-    $('#label').html(Math.round(percnum)+'%');
-  }
-  //ffbinaries.clearCache();
+    function tickerFn(data) {
+        //console.log('\x1b[2m' + data.filename + ': Downloading ' + (data.progress * 100).toFixed(1) + '%\x1b[0m');
+        var elem = document.getElementById("myBar");
+        var percnum = (data.progress * 100 / 2 + 50).toFixed(1);
+        var perc = percnum + '%';
+        elem.style.width = perc;
+        $('#label').html(Math.round(percnum) + '%');
+    }
+    ffbinaries.clearCache();
 
-  var plat = ffbinaries.detectPlatform();
+    var plat = ffbinaries.detectPlatform();
 
-  var options = {
-    platform: plat,
-    quiet: false,
-    destination: ffpath,
-    tickerFn: tickerFn,
-    tickerInterval: 100
-  };
+    var options = {
+        platform: plat,
+        quiet: false,
+        destination: ffpath,
+        tickerFn: tickerFn,
+        tickerInterval: 100
+    };
 
-  ffbinaries.downloadFiles(['ffprobe'], options, function (err, data) {
-    console.log('Downloading ffmpeg binary to ' + ffpath + '.');
-    console.log('err', err);
-    console.log('data', data);
+    ffbinaries.downloadFiles(['ffprobe'], options, function(err, data) {
+        console.log('Downloading ffmpeg binary to ' + ffpath + '.');
+        if (err != null) {
+            console.log('err', err);
+        }
+        console.log('data', data);
 
-    callback(err, data);
-  });
+        callback(err, data);
+    });
 }
 
 
 var url = window.location.pathname;
-var filename = url.substring(url.lastIndexOf('/')+1);
-if (filename=="firstrun.html"){
-    $('#myProgress, #cropview, #progressmsg').show();
-    downloadFFmpeg(function (err, data) {
-        if (err) {
-            console.log('Downloads failed.');
-          }
-              downloadFFprobe(function (err, data) {
-                if (err) {
-                console.log('Downloads failed.');
-                 }
-                var elem = document.getElementById("myBar");
-                var perc = '100%';
-                elem.style.width = perc;
-                $('#label').html('100%');
-                store.set('firstrun','0');
-                setTimeout(function(){ window.location.href = 'index.html';}, 1000);
-            });
-    });
+var filename = url.substring(url.lastIndexOf('/') + 1);
+if (filename == "firstrun.html") {
+    firstRunFF();
+}
+var internetCheckInterval;
+function noInternet(){
+    $("#progressmsg").html('something went wrong, be sure you have internet access');
+        internetCheckInterval = setInterval(internetCheck,1000);
+}
 
+function internetCheck(){
+    console.log(navigator.onLine);
+    if(navigator.onLine){
+        firstRunFF();
+        clearInterval(internetCheckInterval);
+    } else {
+        console.log('checking internet');
+    }
+}
+
+function firstRunFF(){
+        $('#myProgress, #cropview, #progressmsg').show();
+    downloadFFmpeg(function(err, data) {
+        if (err) {
+            noInternet();
+            console.log('Downloads failed:'+err);
+
+        } else {
+            downloadFFprobe(function(err, data) {
+                if (err) {
+                    console.log('Downloads failed.');
+                } else {
+                    var elem = document.getElementById("myBar");
+                    var perc = '100%';
+                    elem.style.width = perc;
+                    $('#label').html('100%');
+                    store.set('firstrun', '0');
+                    setTimeout(function() {
+                        window.location.href = 'index.html';
+                    }, 1000);
+                }
+            });
+        }
+    });
 }
 
 var filelist = [];
@@ -586,7 +615,7 @@ function preview() {
         if (ismac) {
             var ffprobe = spawnsync(ffprobepath, ['-print_format', 'json', '-show_streams', '-select_streams', 'v', '-i', filelist[i]]);
         } else {
-                ffprobe = spawnsync('cmd.exe', ['/c', ffprobepath, '-print_format', 'json', '-show_streams', '-i', filelist[i]]);
+            ffprobe = spawnsync('cmd.exe', ['/c', ffprobepath, '-print_format', 'json', '-show_streams', '-i', filelist[i]]);
         }
         if (ffprobe.status.toString() == 0) {
             var ffprobeOb = JSON.parse(ffprobe.stdout);
