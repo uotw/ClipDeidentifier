f1ac96ad55e2ef28586e46b35ed5069627300132
Ben <bensmith.md@gmail.com>
2020-02-13 21:24:40 -0500

    moved binaries to userData folder

--- js/renderer.js
+++ js/renderer.js
@@ -365,7 +365,7 @@ function progress(i) {
             var basename = path.basename(fullpath, ext);
             var finalcroppedfile = dir + "\\" + basename + "_crop" + ext;
             console.log("trying to write: " + croppedfilelist[i] + " => " + finalcroppedfile);
-            fs.copyFile(croppedfilelist[i],finalcroppedfile);
+            fs.copyFileSync(croppedfilelist[i],finalcroppedfile);
             $('#croplist').append(originals[i] + '=>' + finalcroppedfile + '<br>');
         } else {
             $('#croplist').append(filelist[i] + '=>' + croppedfilelist[i] + '<br>');
